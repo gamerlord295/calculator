ce95eb24a79d5150b9721fea20f8ed8f47d9f2c8
Omar Saad <64932754+gamerlord295@users.noreply.github.com>
2024-01-09 23:33:39 +0200

    project done

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import "./App.css";
+
+import { ReactNode, useEffect, useState } from "react";
+
+import Button from "./Button";
+
+const keys: string[] = [
+  "/",
+  "*",
+  "1",
+  "2",
+  "3",
+  "-",
+  "4",
+  "5",
+  "6",
+  "+",
+  "7",
+  "8",
+  "9",
+  "0",
+  ".",
+];
+
+function App() {
+  const [result, setResult] = useState("");
+  useEffect(() => {
+    console.log(result);
+  }, [result]);
+
+  const getResults = () => {
+    const newResult: string = eval(result);
+    setResult(newResult.toString());
+  };
+
+  return (
+    <>
+      <h1 style={{width: "300px", height: "fit-content"}}>{result || 0}</h1>
+      <div className="grid">
+      <div className="button  del" onClick={() => setResult(prev => prev.slice(0, -1))}>del</div>
+        {keys.map(
+          (key: string): ReactNode => (
+            <Button
+              setResult={setResult}
+              key={key}
+              className={key === "0" ? "zero" : null}
+              label={key}
+            />
+          )
+        )}
+        <div className="button eql" onClick={getResults}>
+          =
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
